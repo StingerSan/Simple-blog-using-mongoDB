b7b94adfebd7ef10f201b6caf54da094315cc2c7
StingerSan <stinger@int.pl>
2022-07-11 18:51:49 +0200

    Connecting to MongoDB (from inside NodeJS)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const blogRoutes = require('./routes/blog');
+const db = require('./data/database');
 
 const app = express();
 
@@ -10,7 +11,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
+app.use(express.urlencoded({
+  extended: true
+})); // Parse incoming request bodies
 app.use(express.static('public')); // Serve static files (e.g. CSS files)
 
 app.use(blogRoutes);
@@ -22,4 +25,6 @@ app.use(function (error, req, res, next) {
   res.status(500).render('500');
 });
 
-app.listen(3000);
+db.connectToDatabase().then(function () {
+  app.listen(3000);
+});
