49bd4d9bfe9438421371bdd32f1208233c999259
StingerSan <stinger@int.pl>
2022-07-17 20:49:55 +0200

    Fetching a single document

--- routes/blog.js
+++ routes/blog.js
@@ -12,7 +12,7 @@ router.get('/', function (req, res) {
 });
 
 router.get('/posts', async function (req, res) {
-  const posts = await db.getDb.collection('posts').find({}, {
+  const posts = await db.getDb().collection('posts').find({}, {
     title: 1,
     summary: 1,
     'author.name': 1
@@ -50,6 +50,17 @@ router.post('/posts', async function (req, res) {
   const result = await db.getDb().collection('posts').insertOne(newPost);
   console.log(result);
   res.redirect('./posts');
+});
+
+router.get('/posts/:id', async function(req, res) {
+  const postId =  req.params.id;
+  const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(postId)}, { summary: 0 });
+
+  if(!post) {
+    return res.status(404).render('404')
+  }
+
+  res.render('post-detail', { post: post});
 });
 
 module.exports = router;
