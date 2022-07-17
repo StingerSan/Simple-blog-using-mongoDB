2afa79f9a500c771f8bb39aa724771a388d4f0f6
StingerSan <stinger@int.pl>
2022-07-17 22:08:38 +0200

    Updating documents

--- routes/blog.js
+++ routes/blog.js
@@ -1,38 +1,45 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.redirect('/posts');
+router.get("/", function (req, res) {
+  res.redirect("/posts");
 });
 
-router.get('/posts', async function (req, res) {
-  const posts = await db.getDb().collection('posts').find({}, {
-    title: 1,
-    summary: 1,
-    'author.name': 1
-  }).toArray();
-  res.render('posts-list', {
-    posts: posts
+router.get("/posts", async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection("posts")
+    .find(
+      {},
+      {
+        title: 1,
+        summary: 1,
+        "author.name": 1,
+      }
+    )
+    .toArray();
+  res.render("posts-list", {
+    posts: posts,
   });
 });
 
-router.get('/new-post', async function (req, res) {
-  const authors = await db.getDb().collection('authors').find().toArray();
-  res.render('create-post', {
-    authors: authors
+router.get("/new-post", async function (req, res) {
+  const authors = await db.getDb().collection("authors").find().toArray();
+  res.render("create-post", {
+    authors: authors,
   });
 });
 
-router.post('/posts', async function (req, res) {
+router.post("/posts", async function (req, res) {
   const authorId = new ObjectId(req.body.author);
-  const author = await db.getDb().collection('authors').findOne({
-    _id: authorId
+  const author = await db.getDb().collection("authors").findOne({
+    _id: authorId,
   });
 
   const newPost = {
@@ -43,32 +50,69 @@ router.post('/posts', async function (req, res) {
     author: {
       id: authorId,
       name: author.name,
-      email: author.email
-    }
+      email: author.email,
+    },
   };
 
-  const result = await db.getDb().collection('posts').insertOne(newPost);
+  const result = await db.getDb().collection("posts").insertOne(newPost);
   console.log(result);
-  res.redirect('./posts');
+  res.redirect("./posts");
 });
 
-router.get('/posts/:id', async function(req, res) {
-  const postId =  req.params.id;
-  const post = await db.getDb().collection('posts').findOne({_id: new ObjectId(postId)}, { summary: 0 });
+router.get("/posts/:id", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
 
-  if(!post) {
-    return res.status(404).render('404')
+  if (!post) {
+    return res.status(404).render("404");
   }
 
-  post.humanReadableDate = post.date.toLocaleDateString('en-US', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
+  post.humanReadableDate = post.date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   });
   post.date = post.date.toISOString();
 
-  res.render('post-detail', { post: post});
+  res.render("post-detail", { post: post });
+});
+
+router.get("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { title: 1, summary: 1, body: 1 });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  res.render("update-post", { post: post });
+});
+
+router.post("/posts/:id/edit", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const result = await db
+    .getDb()
+    .collection("posts")
+    .updateOne(
+      { _id: postId },
+      {
+        $set: {
+          title: req.body.title,
+          summary: req.body.summary,
+          body: req.body.content,
+          date: new Date(),
+        },
+      }
+    );
+
+  res.redirect("/posts");
 });
 
 module.exports = router;
